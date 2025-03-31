0ea86b38c393d9d54c7752079ca5b9540481029f
orrodguez19 <miguelorlandos97@gmail.com>
2025-03-31 13:09:55 -0400

    Actualizar app.py

--- app.py
+++ app.py
@@ -160,4 +160,4 @@ async def websocket_endpoint(websocket: WebSocket, username: str):
         manager.disconnect(username)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
