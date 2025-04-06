24511bf189947ed9b7e93317c1c381c7bfbdd3d9
orrodguez19 <miguelorlandos97@gmail.com>
2025-04-06 10:49:42 -0400

    Actualizar app.py

--- app.py
+++ app.py
@@ -152,5 +152,5 @@ def handle_message(data):
         emit('new_message', message_data, broadcast=True)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     socketio.run(app, host='0.0.0.0', port=port)
