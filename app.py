5da44d93e67bc7af966fc91e0e42450e73ded1fd
orrodguez19 <miguelorlandos97@gmail.com>
2025-04-06 18:25:38 -0400

    Actualizar app.py

--- app.py
+++ app.py
@@ -199,4 +199,4 @@ def handle_message(data):
     emit('chats_update', get_chats().json, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
